7a47bc2ba9151f22ece6bbbfa6ef2f29ad700757
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-10-07 09:57:03 -0400

    create application class and empty array of 20 books..

--- src/main/java/com/Pluralsight/BookStore.java
+++ src/main/java/com/Pluralsight/BookStore.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class BookStore {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
+        Book[] books= new Book[20];
+
+        for (Book book: books) {
+            System.out.println(book);
+        }
 
 
     }
